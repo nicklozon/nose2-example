d4213f8ae02769fb18cd02c04b1f2b97c082162b
Nick Lozon <nicklozon@gmail.com>
2017-07-20 22:43:30 -0400

    Initial commit.

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import unittest
+
+from app import app
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        assert rv.data == b'Hello World'
+
+
+if __name__ == '__main__':
+    unittest.main()
